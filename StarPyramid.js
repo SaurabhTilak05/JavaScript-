98756e7db58cf04a4972938b1668d5c1c29f0c18
SAURABHTILAK <saurbhtilak6@gmail.com>
2025-04-17 22:46:23 +0530

    Draw Star pyramid

---
+++ StarPyramid.js
@@ -0,0 +1,14 @@
+let pattern="";
+for(let i=1;i<6;i++)
+{
+	for(let j=1;j<6-i;j++)
+	{
+		pattern+=" ";
+	}
+	for(let k=1;k<=2*i-1;k++)
+	{
+		pattern+="*";
+	}
+	pattern+="\n";
+}
+console.log(pattern);
