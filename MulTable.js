a32771b04664a923a0026b708a7dea72bb9482f6
SAURABHTILAK <saurbhtilak6@gmail.com>
2025-04-25 12:39:48 +0530

    Print Multiplication Table of A Number

---
+++ MulTable.js
@@ -0,0 +1,14 @@
+/*	 Multiplication Table
+	Description: Write a program to print the multiplication table for a number n.
+	Input: n = 4
+	Output: 4 8 12 16 20
+*/
+
+let n=4;
+
+for(let i=1;i<=10;i++)
+{
+	let tab=n*i;
+	console.log(tab);
+}
+
