069f36a526504041efc5ed797e0233f63726c584
SAURABHTILAK <saurbhtilak6@gmail.com>
2025-04-25 22:45:43 +0530

    calculate sum of the first n even numbers

---
+++ SumOfFirstEvenNum.js
@@ -0,0 +1,21 @@
+/*
+	Sum of First N Even Numbers
+	Description: Write a program to calculate the sum of the first n even numbers.
+	Input: n = 4
+	Output: 20
+*/
+
+let n=4;
+let i=1;
+let count=0;
+let sum=0;
+while(count<n)
+{
+	if(i%2==0)
+	{
+		sum=sum+i;
+		count++;
+	}
+	i++;
+}
+console.log("Sum is:"+sum);
