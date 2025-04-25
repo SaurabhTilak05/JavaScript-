5b790e5888d011593dd2d4290dc78a31d122b8c2
SAURABHTILAK <saurbhtilak6@gmail.com>
2025-04-25 12:59:05 +0530

    Print Odd Number Up to Nth

---
+++ OddNumber.js
@@ -0,0 +1,15 @@
+/*
+	Odd Numbers
+	Description: Write a program to print all odd numbers between 1 and n.
+	Input: n = 10
+	Output: 1 3 5 7 9
+*/
+
+let no=10;
+for(let i=1;i<=no;i++)
+{
+	if(i%2==1)
+	{
+		console.log(i);
+	}
+}
