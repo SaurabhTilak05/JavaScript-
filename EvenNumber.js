6a89ad8490b385f1840739b1cc6d5e9e00d0454c
SAURABHTILAK <saurbhtilak6@gmail.com>
2025-04-25 12:58:18 +0530

    Print Even Number Up to n

---
+++ EvenNumber.js
@@ -0,0 +1,16 @@
+/*
+	Even Numbers
+	Description: Write a program to print all even numbers between 1 and n.
+	Input: n = 10
+	Output: 2 4 6 8 10
+
+*/
+
+let no=10;
+for(let i=1;i<=no;i++)
+{
+	if(i%2==0)
+	{
+		console.log(i);
+	}
+}
