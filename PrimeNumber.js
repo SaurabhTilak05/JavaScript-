37cf0b706837352f61dd78bc3d21d89ab479bc57
SAURABHTILAK <saurbhtilak6@gmail.com>
2025-04-25 11:31:58 +0530

    Print Prime numbers up to n

---
+++ PrimeNumber.js
@@ -0,0 +1,23 @@
+/*Prime Numbers
+Description: Write a program to print all prime numbers up to a given number n.
+Input: n = 10
+Output: 2 3 5 7
+*/
+
+let no=10;
+for (let i=2;i<=no;i++)
+{
+	let flag = true;
+	for(let j=2;j<=i/2;j++)
+	{
+		if(i%j===0)
+		{
+			flag=false;
+			break;
+		}
+	}
+	if(flag)
+	{
+		console.log(i);
+	}
+}
