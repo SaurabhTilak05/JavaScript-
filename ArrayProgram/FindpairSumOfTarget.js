9a9a45216542ed13edef3487c0148404ea94b382
SaurabhTilak <saurbhtilak6@gmail.com>
2025-05-25 15:05:52 +0530

    Find All Pairs with a Given Sum

---
+++ ArrayProgram/FindpairSumOfTarget.js
@@ -0,0 +1,18 @@
+/*Find All Pairs with a Given Sum
+Question:
+ Find all pairs in an array whose sum is equal to a target value.
+ Example:
+ Input: arr = [1, 3, 2, 2, 4, 0], sum = 4
+ Output: [[1,3],[2,2],[4,0]]
+Explanation:
+ Use two nested loops to check if any pair adds up to the target.
+*/
+let ar = [1, 3, 2, 2, 4, 0];
+let elm = 4;
+for (let i = 0; i < ar.length; i++) {
+    for (let j = i + 1; j < ar.length; j++) {
+        if (ar[i] + ar[j] == elm) {
+            console.log(ar[i], ar[j]);
+        }
+    }
+}
