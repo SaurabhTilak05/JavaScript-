e88549bed3d23ec6beaa61d5952287edcc9c37cf
SaurabhTilak <saurbhtilak6@gmail.com>
2025-05-26 08:50:02 +0530

    Rotate array to the right side by kth position

--- ArrayProgram/RotateArrByPosition.js
+++ ArrayProgram/RotateArrByPosition.js
@@ -1,17 +1,19 @@
-/*
-	Rotate Array by N Positions
-	Description: Rotate the array by n positions to the right.
-	Input: [1, 2, 3, 4, 5], n = 2
-	Output: [4, 5, 1, 2, 3]
-	
+/*Rotate an Array by k Positions
+Question:
+ Write a function that rotates an array to the right by k steps.
+ Example:
+ Input: arr = [1,2,3,4,5], k = 2 → Output: [4,5,1,2,3]
+Explanation:
+ To rotate right, take the last k elements and move them to the front. Use slicing and concatenation.
 */
-let ar=[1, 2, 3, 4, 5];
-let p=2;
-for(let i=p;i<ar.length;i++)
-{	
-	console.log(ar[i]);
+let ar = [1, 2, 3, 4, 5];
+let k = 2;
+for (let i = 0; i < k; i++) {
+    let str = ar[0];
+    for (let j = 0; j < ar.length - 1; j++) {
+        ar[j] = ar[j + 1];
+
+    }
+    ar[ar.length - 1] = str;
 }
-for(let i=0;i<p;i++)
-{
-	console.log(ar[i]);
-}
+console.log(ar);
